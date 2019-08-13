f025b5c9c69e1962ff2c0dc0467054f9d42e57d2
Joseph Bauer <root@thesudorm.com>
2019-08-12 22:06:08 -0400

    Everything before the first lesson.
    Just some comments about what different parts of the code do.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  selector: 'my-app', // to use me in html: <my-app>
+  template: `<h1>Hello {{name}}</h1>`, // templates are fragments, not whole html doc
 })
-export class AppComponent  { name = 'Angular'; }
+
+// export lets other parts of the code to use it
+export class AppComponent  { 
+    name = 'Angular'; 
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 
+// this desribes the class that follows it
+// this is a decorator
+// decorator thing
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule ], // What do I need?
+  declarations: [ AppComponent ], // what things are in my app?
+  bootstrap:    [ AppComponent ] // Where do I start?
 })
+
 export class AppModule { }
